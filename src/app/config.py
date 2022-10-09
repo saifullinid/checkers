95e1fde825aa25d017ebecfa0aa1aef4ba236e4d
saifullinid <saifullin.id@gmail.com>
2022-10-09 23:39:02 +0500

    config db fix

--- src/app/config.py
+++ src/app/config.py
@@ -8,5 +8,5 @@ STATIC_DIR = os.path.abspath('../dist')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flasksql'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://sid:12345@localhost/checkers'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
